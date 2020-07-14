7c1f98d0a27821bc48f372a179f8a617da83a1a0
tony <wabslygzj@163.com>
2020-07-14 16:57:28 +0800

    Param type definition and transfer

---
+++ src/com/tony/kotlin/HelloKotlin2.kt
@@ -0,0 +1,20 @@
+package com.tony.kotlin
+
+fun main(args: Array<String>) {
+    val a: Int = 1
+    val b: Int
+    //val can't be reassigned
+//    a=3
+    var c: Int = 3
+    c = 5
+    /**
+     *
+     */
+    /*
+
+     */
+    var x = 10
+    var y: Byte = 20
+    x = y.toInt()
+    println(x)
+}
